93b631098544d898632b99e5baf023735f09144e
will <denis.barbosa@fatec.sp.gov.br>
2022-08-21 00:39:42 -0300

    Created Categorias entiy

--- src/main/java/br/com/wot/resources/CategoriaResoucers.java
+++ src/main/java/br/com/wot/resources/CategoriaResoucers.java
@@ -1,16 +1,28 @@
 package br.com.wot.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.wot.domain.Categorias;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResoucers {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Listou rest";
+	public List<Categorias> listar() {
+		
+		Categorias cat1 = new Categorias(1, "Informatica");
+		Categorias cat2 = new Categorias(2, "Escritorio");
+		
+		List<Categorias> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
-
 }
