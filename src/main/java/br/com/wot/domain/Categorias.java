5f3cf84995eb71f7f3af0a8c267bf80f4e58221b
will <denis.barbosa@fatec.sp.gov.br>
2022-08-23 17:35:41 -0300

    Atualizacao

---
+++ src/main/java/br/com/wot/domain/Categorias.java
@@ -0,0 +1,32 @@
+package br.com.wot.domain;
+
+import java.io.Serializable;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(exclude= {"nome"})
+@Entity
+public class Categorias implements Serializable {	
+	private static final long serialVersionUID = 1L;
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+}
