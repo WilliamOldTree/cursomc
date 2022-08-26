d8cdf43fd3587d452fe15c44c587f46ad3880f85
William Barbosa <dwbarbosa@rd.com.br>
2022-08-26 09:26:08 -0300

    criacao repositores e services

---
+++ src/main/java/br/com/wot/domain/Categoria.java
@@ -0,0 +1,66 @@
+package br.com.wot.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@Entity
+public class Categoria implements Serializable {	
+	
+	private static final long serialVersionUID = 1L;
+	
+	//atributos	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+
+	
+	//construtores
+	public Categoria() {
+		
+	}
+	public Categoria(Long id, String nome) {
+		super();
+		this.id = id;
+		this.setNome(nome);
+	}
+	
+					//METODOS
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(id, other.id);
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}//end class
--- src/main/java/br/com/wot/resources/CategoriaResoucers.java
+++ src/main/java/br/com/wot/resources/CategoriaResoucers.java
@@ -1,28 +1,48 @@
 package br.com.wot.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.wot.domain.Categorias;
+import br.com.wot.domain.Categoria;
+import br.com.wot.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResoucers {
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Categorias> listar() {
-		
-		Categorias cat1 = new Categorias(null, "Iformatica");
-		Categorias cat2 = new Categorias(null, "Escritorio");
-		
-		List<Categorias> list = new ArrayList<>();
-		list.add(cat1);
-		list.add(cat2);
-		
-		return list;
+	//injecao dependencia
+	@Autowired
+	private CategoriaService service;
+	
+	
+							//METODOS	
+	//busca por Id
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Categoria> findById(@PathVariable Long id){
+		Categoria obj = service.findById(id);
+		return ResponseEntity.ok(obj);
 	}
-}
+	
+	//listar categorias
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<Categoria>> findAll(){
+		List<Categoria>list=service.findAll();
+		return ResponseEntity.ok(list);
+	}
+	
+	//atualizar
+	
+	//deletar
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> deletar(@PathVariable Long id){
+		service.deletar(id);
+		return ResponseEntity.noContent().build();
+	}
+
+}//end class
